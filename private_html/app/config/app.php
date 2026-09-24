4392cd861bd8155c05826c54e1511a92a07d7dc7
dsoos1290 <d@soos1290.com>
2026-09-24 07:16:12 +0200

    #1

--- private_html/app/config/app.php
+++ private_html/app/config/app.php
@@ -1,10 +1,10 @@
 <?php
-// define('APP_NAME', 'VC Framework');
-// define('APP_PROJECT_URL', 'https://github.com/dsoos1290/vcframework');
+define('APP_NAME', 'BlogSite CMS');
+define('APP_PROJECT_URL', 'https://github.com/dsoos1290/blogsitecms');
 define('APP_VER', '0.1.0');
 define('APP_TZ', 'UTC');
 define('APP_ENV', 'dev'); // prod
 define('APP_LANG', 'en');
-define('APP_TITLE', 'App');
+define('APP_TITLE', 'BlogSite CMS');
 define('APP_SESSION', true); // flash
 // define('APP_URL', 'https://example.com');
